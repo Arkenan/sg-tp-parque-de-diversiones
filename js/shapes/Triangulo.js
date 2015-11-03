c0b8118a5f9c755c7e0d6372ad46c681b00b11c0
Tomas Arjovsky <t.arjovsky@gmail.com>
2015-11-03 03:20:18 -0300

    precómputo de algunas variables. Se podrá hacer con las matrices?

--- js/shapes/Triangulo.js
+++ js/shapes/Triangulo.js
@@ -4,35 +4,40 @@ module.exports = function(){
     this.izq = null;
     this.der = null;
     this.ab = null;
+    this.theta = null;
+    this.largo_op = null;
+    this.d_op = null;
 
     this.init = function(gl){
         this.izq = new Cilindro(5,2).init(gl);
         this.der = new Cilindro(5,2).init(gl);
         this.ab = new Cilindro(5,2).init(gl);
+        this.theta = 2*Math.PI/15;
+        this.largo_op = 10*Math.sin(this.theta/2);
+        this.d_op = 5*Math.cos(this.theta/2),0;
         return this;
     }
 
     this.draw = function(gl, program, mv){
         var mizq = mat4.create(), mder = mat4.create(), mab = mat4.create();
-        var theta = 2*Math.PI/15;
 
         // Recordar que se escribe (output, input, ...)
-        mat4.rotate(mizq, mv, -theta/2, [0,0,1]);
+        mat4.rotate(mizq, mv, -this.theta/2, [0,0,1]);
         mat4.rotate(mizq, mizq, Math.PI/2, [1,0,0]);
         mat4.rotate(mizq, mizq, Math.PI/4, [0,0,1]);
         mat4.scale(mizq, mizq, [0.1, 0.1, 5]);
         this.izq.draw(gl,program,mizq);
 
-        mat4.rotate(mder, mv, theta/2, [0,0,1]);
+        mat4.rotate(mder, mv, this.theta/2, [0,0,1]);
         mat4.rotate(mder, mder, Math.PI/2, [1,0,0]);
         mat4.rotate(mder, mder, Math.PI/4, [0,0,1]);
         mat4.scale(mder, mder, [0.1, 0.1, 5]);
         this.der.draw(gl,program,mder);
 
-        mat4.translate(mab, mv,[-5*Math.sin(theta/2),-5*Math.cos(theta/2),0]);
+        mat4.translate(mab, mv,[-this.largo_op/2,-this.d_op,0]);
         mat4.rotate(mab, mab, Math.PI/2, [0,1,0]);
         mat4.rotate(mab, mab, Math.PI/4, [0,0,1]);
-        mat4.scale(mab, mab, [0.1, 0.1, 10*Math.sin(theta/2)]);
+        mat4.scale(mab, mab, [0.1, 0.1, this.largo_op]);
         this.ab.draw(gl,program,mab);
 
     }
