ec4d18de9088724b8d7750ece0cf0eb81915274e
Tomas Arjovsky <t.arjovsky@gmail.com>
2015-11-03 04:48:27 -0300

    Armazón de la vuelta al mundo. Usa triángulos de forma compuesta.

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ var FragmentShader = require('./shaders/FragmentShader.js');
 var Program = require('./program/Program.js');
 //------------------------------------------------------------------------------------------------------------------------------
 //var Grid = require('./shapes/Grid.js');
-var Plano = require('./shapes/Plano.js');
-var Cilindro = require('./shapes/Cilindro.js')
+//var Plano = require('./shapes/Plano.js');
+//var Cilindro = require('./shapes/Cilindro.js');
+var Armazon = require('./shapes/Armazon.js');
 //------------------------------------------------------------------------------------------------------------------------------
 window.onload = function(){
   var scene = document.createElement('canvas');
@@ -27,19 +28,12 @@ window.onload = function(){
     var fragment = new FragmentShader().init(gl);
     var program  = new Program().init(gl,vertex,fragment);
 
-    var cil = new Cilindro(30,4).init(gl);
-    var plano = new Plano(2,3).init(gl);
-
-    // Matrices de modelado de las figuras.
-    var mvPlano = mat4.create();
-    mat4.identity(mvPlano);
-    mat4.translate(mvPlano, mvPlano, [0.0, 0.0, -5.0]);
-
-    var mvCilindro = mat4.create();
-    mat4.identity(mvCilindro);
-    mat4.translate(mvCilindro, mvCilindro, [0.0, 0.0, -5.0]);
-
-    //Matriz de proyección perspectiva.
+    var armazon = new Armazon().init(gl);
+    var mv = mat4.create();
+    mat4.identity(mv);
+    mat4.translate(mv, mv, [0.0, 0.0, -15.0]);
+    mat4.rotate(mv,mv,Math.PI/4,[0,1,0]);
+    // Matriz de proyección perspectiva.
     var pMatrix = mat4.create();
     var t = 0.0;
 
@@ -54,14 +48,7 @@ window.onload = function(){
       var u_proj_matrix = gl.getUniformLocation(program, "uPMatrix");
       gl.uniformMatrix4fv(u_proj_matrix, false, pMatrix);
 
-      // Rotación del plano.
-      mat4.rotate(mvPlano, mvPlano, 0.01, [0.0, 1.0, 0.0]);
-
-      // Rotación del Cilindro.
-      mat4.rotate(mvCilindro, mvCilindro, -0.01, [0.0, 1.0, 0.0]);
-
-      cil.draw(gl,program,mvCilindro);
-      plano.draw(gl,program, mvPlano);
+      armazon.draw(gl,program,mv);
     }
     setInterval(drawScene, 10);
 
