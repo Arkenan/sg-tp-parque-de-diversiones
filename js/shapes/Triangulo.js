fce054d130b1cd8b7452aab679b723739902e72b
Tomas Arjovsky <t.arjovsky@gmail.com>
2015-11-03 04:47:13 -0300

    mejoras de sintaxis y prolijidad

--- js/shapes/Triangulo.js
+++ js/shapes/Triangulo.js
@@ -1,20 +1,13 @@
 var Cilindro = require("./Cilindro.js");
 
 module.exports = function(){
-    this.izq = null;
-    this.der = null;
-    this.ab = null;
-    this.theta = null;
-    this.largo_op = null;
-    this.d_op = null;
+    this.cil = null;
+    this.theta = 2*Math.PI/15;
+    this.largo_op = 10*Math.sin(this.theta/2);
+    this.d_op = 5*Math.cos(this.theta/2),0;
 
     this.init = function(gl){
-        this.izq = new Cilindro(5,2).init(gl);
-        this.der = new Cilindro(5,2).init(gl);
-        this.ab = new Cilindro(5,2).init(gl);
-        this.theta = 2*Math.PI/15;
-        this.largo_op = 10*Math.sin(this.theta/2);
-        this.d_op = 5*Math.cos(this.theta/2),0;
+        this.cil = new Cilindro(5,2).init(gl);
         return this;
     }
 
@@ -26,19 +19,19 @@ module.exports = function(){
         mat4.rotate(mizq, mizq, Math.PI/2, [1,0,0]);
         mat4.rotate(mizq, mizq, Math.PI/4, [0,0,1]);
         mat4.scale(mizq, mizq, [0.1, 0.1, 5]);
-        this.izq.draw(gl,program,mizq);
+        this.cil.draw(gl,program,mizq);
 
         mat4.rotate(mder, mv, this.theta/2, [0,0,1]);
         mat4.rotate(mder, mder, Math.PI/2, [1,0,0]);
         mat4.rotate(mder, mder, Math.PI/4, [0,0,1]);
         mat4.scale(mder, mder, [0.1, 0.1, 5]);
-        this.der.draw(gl,program,mder);
+        this.cil.draw(gl,program,mder);
 
         mat4.translate(mab, mv,[-this.largo_op/2,-this.d_op,0]);
         mat4.rotate(mab, mab, Math.PI/2, [0,1,0]);
         mat4.rotate(mab, mab, Math.PI/4, [0,0,1]);
         mat4.scale(mab, mab, [0.1, 0.1, this.largo_op]);
-        this.ab.draw(gl,program,mab);
+        this.cil.draw(gl,program,mab);
 
     }
 
