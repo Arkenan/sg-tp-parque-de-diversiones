4a75581f4c1c47d9cdacafce57940e4de81ea351
Christian Angelone <christiangelone@gmail.com>
2015-11-24 09:40:41 -0800

    Adds Phong model over FragmentShader and VertexShader

--- js/shaders/VertexShader.js
+++ js/shaders/VertexShader.js
@@ -3,13 +3,25 @@ module.exports = function () {
     var vertex = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertex,[
       'attribute vec3 aVertexPosition;',
-      'attribute vec3 aVertexColor;',
+      'attribute vec3 aVertexNormal;',
+
       'uniform mat4 uMVMatrix;',
       'uniform mat4 uPMatrix;',
-      'varying highp vec4 vColor;',
+      'uniform mat4 uNMatrix;',
+
+      'varying vec3 vNormal;',
+      'varying vec3 vEyeVec;',
+
       'void main(void){',
-          'gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);',
-          'vColor = vec4(aVertexColor,1.0);',
+        //Transformed vertex position
+        'vec4 vertex = uMVMatrix * vec4(aVertexPosition, 1.0);',
+
+        //Transformed normal position
+        'vNormal = vec3(uNMatrix * vec4(aVertexNormal, 1.0));',
+
+        //Vector Eye
+        'vEyeVec = -vec3(vertex.xyz);',
+        'gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);',
       '}'
     ].join('\n'));
     gl.compileShader(vertex);
