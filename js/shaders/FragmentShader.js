05f5869f8de2c8c1720cf480f1dfc100519726a9
Christian Angelone <christiangelone@gmail.com>
2015-11-28 16:47:23 -0800

    Adds the color to each fragment

--- js/shaders/FragmentShader.js
+++ js/shaders/FragmentShader.js
@@ -17,6 +17,8 @@ module.exports = function() {
       'uniform vec4 uMaterialDiffuse;  //object diffuse property',
       'uniform vec4 uMaterialSpecular; //object specular property',
 
+      'varying highp vec4 vColor;',
+
       'varying vec3 vNormal;',
       'varying vec3 vEyeVec;',
 
@@ -46,7 +48,7 @@ module.exports = function() {
         '}',
 
         '//Final color',
-        'vec4 finalColor = Ia + Id + Is;',
+        'vec4 finalColor = Ia + Id + Is + vColor;',
         'finalColor.a = 1.0;',
 
         'gl_FragColor = finalColor;',
