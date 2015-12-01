f1921cc8f403eb9954199ef6b975fa7fb14f79c4
Tomas Arjovsky <t.arjovsky@gmail.com>
2015-12-01 15:44:41 -0300

    arreglo normales del Durmiente (estaba horario en lugar de antihorario el orden de los vértices

--- js/models/montania_rusa/shapes/Durmiente.js
+++ js/models/montania_rusa/shapes/Durmiente.js
@@ -1,7 +1,7 @@
 var BarridoD = require("../../../main_shapes/BarridoDiscreto.js");
 
 module.exports = function(){
-  this.vertices = [0,-1/6,0,-1,0.5,0,0,1/7,0,1,0.5,0,0,-1/6,0];
+  this.vertices = [0,-1/6,0,1,0.5,0,0,1/7,0,-1,0.5,0,0,-1/6,0];
 
   this.fBarrido = function(t){
     return [0, 0, t];
